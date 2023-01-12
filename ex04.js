315c575c284368e60473182c4b23a80722adbbe1
Jonathan Henrique <110499094+Jonathan-Henriq@users.noreply.github.com>
2023-01-12 20:48:25 -0300

    Create ex04.js

---
+++ ex04.js
@@ -0,0 +1,10 @@
+var idade = 18
+console.log (`você tem ${idade} anos`)
+if (idade ==16 || idade ==17 || idade >65) {
+    console.log ('Voto Opcional')
+}  else {
+        console.log('Voto Obrigatorio')
+   
+}
+
+    
